17b77c374d4a0a8eaed4f08f1bdcf42922b3930c
Vuyisile <vtlehola23@gmail.com>
2024-01-04 09:26:13 -0800

    program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,26 +5,27 @@ from calculator_1 import add, sub, mul, div
 length_of_args = len(sys.argv[1:])
 args = sys.argv[1:]
 
-if length_of_args < 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+if __name__ == "__main__":
+    if length_of_args < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-first_arg = int(args[0])
-operand = args[1]
-second_arg = int(args[2])
+    first_arg = int(args[0])
+    operand = args[1]
+    second_arg = int(args[2])
 
-if operand == '+':
-    print("{} + {} = {}".format(first_arg, second_arg, add(first_arg, second_arg)))
-    sys.exit(0)
-elif operand == '-':
-    print("{} - {} = {}".format(first_arg, second_arg, sub(first_arg, second_arg)))
-    sys.exit(0)
-elif operand == '*':
-    print("{} * {} = {}".format(first_arg, second_arg, mul(first_arg, second_arg)))
-    sys.exit(0)
-elif operand == '*':
-    print("{} / {} = {}".format(first_arg, second_arg, div(first_arg, second_arg)))
-    sys.exit(0)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if operand == '+':
+        print("{} + {} = {}".format(first_arg, second_arg, add(first_arg, second_arg)))
+        sys.exit(0)
+    elif operand == '-':
+        print("{} - {} = {}".format(first_arg, second_arg, sub(first_arg, second_arg)))
+        sys.exit(0)
+    elif operand == '*':
+        print("{} * {} = {}".format(first_arg, second_arg, mul(first_arg, second_arg)))
+        sys.exit(0)
+    elif operand == '*':
+        print("{} / {} = {}".format(first_arg, second_arg, div(first_arg, second_arg)))
+        sys.exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
