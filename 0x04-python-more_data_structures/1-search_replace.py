f88a71ec70656920f54bee32716cf1b03a03fe7b
Vuyisile <vtlehola23@gmail.com>
2024-01-10 10:03:12 -0800

    remove unecessary comments

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
-# loop through list
-# check for 2, if found, replace it with 89
-
 def search_replace(my_list, search, replace):
     list_copy = my_list[:]
-
-    if len(list_copy) < 1:
-        return (None)
 
     for i in range(len(list_copy) - 1):
         if list_copy[i] == search:
