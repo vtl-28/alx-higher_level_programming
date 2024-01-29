66a13ea1a887ad94cf0d5aff37d908b7bb143ea0
Vuyisile <vtlehola23@gmail.com>
2024-01-29 07:13:24 -0800

    remove extra blankspace

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -37,7 +37,6 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
-
         if value < 0:
             raise ValueError("width must be >= 0")
 
