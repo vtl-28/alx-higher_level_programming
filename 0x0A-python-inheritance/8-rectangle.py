da12587fa1929f40578ad089732a2e4b9180f46a
Vuyisile <vtlehola23@gmail.com>
2024-02-05 23:11:02 -0800

    add docstring

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,8 +8,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """A representation of a rectangle"""
 
     def __init__(self, width, height):
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
 
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,7 +8,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """A class with public instance methods area and integer_validator"""
     def __init__(self, width, height):
+        """ instantion of class"""
         self.integer_validator("width", width)
         self.__width = width
 
@@ -16,7 +18,9 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        """raises an exception when called"""
         return self.__width * self.__height
 
     def __str__(self):
+        """prints string representation of object"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
