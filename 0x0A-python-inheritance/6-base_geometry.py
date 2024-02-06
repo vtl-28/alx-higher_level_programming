0036d8ca658677b034ad4e30a2aa57b58646c9c4
Vuyisile <vtlehola23@gmail.com>
2024-02-05 22:58:37 -0800

    add docstring

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+"""
+Contains the class BaseGeometry
+"""
 
-class BaseGeometry():
 
+class BaseGeometry:
+    """A class with public attribute area"""
     def __init__(self):
         pass
 
     def area(self):
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
