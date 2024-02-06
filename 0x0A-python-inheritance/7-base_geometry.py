5bd4d0b3a05725f0afd90ca78b42cab0305ca652
Vuyisile <vtlehola23@gmail.com>
2024-02-05 23:05:11 -0800

    add docstring

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
+"""
+Contains the class BaseGeometry
+"""
 
-class BaseGeometry():
 
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
     def __init__(self):
         pass
 
     def area(self):
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
