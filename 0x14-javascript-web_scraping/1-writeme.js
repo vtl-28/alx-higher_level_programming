58db6581d634709ba27a97e2d928368d1f1b2440
Vuyisile <vtlehola23@gmail.com>
2024-05-21 15:04:34 +0200

    fix error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(file, content, 'utf-8', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log("successful");
+fs.writeFile(file, content, 'utf-8', (error) => {
+    if (error) console.log(error);
 })
